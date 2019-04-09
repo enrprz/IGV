9b71499e935b2e4947515195b9f33dbea6da67e9
johnPOrtiz <johnpaulortiz1997@gmail.com>
2019-04-08 22:00:04 -0500

    Lane Detection - added file README, & updated lane.py(4 new functions - undistort_img(), undistort(), distortion_Correction(), perspective_Warp())

--- lane_detection/lane.py
+++ lane_detection/lane.py
@@ -1,4 +1,92 @@
 #!/env/ python3
 
-def RGB2BGR(img):
-	return img
+
+#Dependencies
+import numpy as np
+import cv2
+import matplotlib
+
+
+
+def undistort_img():
+    # Prepare object points 0,0,0 ... 8,5,0
+    obj_pts = np.zeros((6*9,3), np.float32)
+    obj_pts[:,:2] = np.mgrid[0:9, 0:6].T.reshape(-1,2)
+    # Stores all object points & img points from all images
+    objpoints = []
+    imgpoints = []
+    # Get directory for all calibration images
+    images = glob.glob('camera_cal/*.jpg')
+    for indx, fname in enumerate(images):
+        img = cv2.imread(fname)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        ret, corners = cv2.findChessboardCorners(gray, (9,6), None)
+        if ret == True:
+            objpoints.append(obj_pts)
+            imgpoints.append(corners)
+    # Test undistortion on img
+    img_size = (img.shape[1], img.shape[0])
+    # Calibrate camera
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size, None,None)
+    dst = cv2.undistort(img, mtx, dist, None, mtx)
+    # Save camera calibration for later use
+    dist_pickle = {}
+    dist_pickle['mtx'] = mtx
+    dist_pickle['dist'] = dist
+    pickle.dump( dist_pickle, open('camera_cal/cal_pickle.p', 'wb') )
+
+
+
+def undistort(img, cal_dir='camera_cal/cal_pickle.p'):
+    #cv2.imwrite('camera_cal/test_cal.jpg', dst)
+    with open(cal_dir, mode='rb') as f:
+        file = pickle.load(f)    mtx = file['mtx']
+    dist = file['dist']
+    dst = cv2.undistort(img, mtx, dist, None, mtx)
+    return dst
+
+
+
+def distortion_Correction():	
+	undistort_img()
+	img = cv2.imread('camera_cal/calibration1.jpg')
+	dst = undistort(img) # Undistorted image
+
+	
+
+def perspective_Warp(img,
+                     dst_size=(1280,720),
+                     src=np.float32([(0.43,0.65),(0.58,0.65),(0.1,1),(1,1)]),
+                     dst=np.float32([(0,0), (1, 0), (0,1), (1,1)])):
+    img_size = np.float32([(img.shape[1],img.shape[0])])
+
+    src = src* img_size
+    # For destination points, I'm arbitrarily choosing some points to be
+    # a nice fit for displaying our warped result
+    # again, not exact, but close enough for our purposes
+    dst = dst * np.float32(dst_size)
+    # Given src and dst points, calculate the perspective transform matrix
+    M = cv2.getPerspectiveTransform(src, dst)
+    # Warp the image using OpenCV warpPerspective()
+    warped = cv2.warpPerspective(img, M, dst_size)
+    return warped
+	
+
+def sobel_Filter():
+
+
+def histogramPeak_Det():
+
+
+def slidingWindow_Search():
+
+
+def curve_Fitting():
+
+
+def detectedLane_Overlay():
+
+
+
+
+
